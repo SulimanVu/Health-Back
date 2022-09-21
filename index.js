39ee93892a6afcf9c988f71af7faa28f75983188
errorby <mdidayev@inbox.ru>
2022-09-21 14:52:23 +0300

    Сделал юзеров

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -9,7 +8,8 @@ const path = require("path");
 
 app.use(cors());
 app.use(express.json());
-app.use(require('./routes/product.route'))
+app.use(require('./routes/product.route'));
+app.use(require('./routes/user.route'));
 
 mongoose
   .connect(process.env.MONGO_SERVER)
