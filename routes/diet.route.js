8a6ffd3421094355f7cd5902c7b9086c2a5bdaba
Ashwash <latar14@bk.ru>
2022-09-21 14:18:45 +0300

    Исправил роут и контроллер

--- routes/diet.route.js
+++ routes/diet.route.js
@@ -1,11 +1,10 @@
 const { Router } = require("express");
-const { route } = require("../../../Неделя 11/Truck-Back/routes/request.route");
 const router = Router();
 const { diet } = require("../controllers/diet.controller");
 
 router.get("/diet", diet.getDiet);
 router.post("/diet", diet.addDiet);
-router.get("/diet/:id", diet.updateDiet);
-router.get("/diet/:id", diet.deleteDiet);
+router.patch("/diet/:id", diet.updateDiet);
+router.delete("/diet/:id", diet.deleteDiet);
 
 module.exports = router;
